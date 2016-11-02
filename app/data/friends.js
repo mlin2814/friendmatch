5d9dc576a6e4fa684c098652c056311b0e88482e
Matthew Lin <Matthew Lin>
2016-11-02 10:00:04 -0400

    Laid out foundation for html and js with planned development for matching function

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,50 +1,69 @@
+ var config = {
+    '.chosen-select': {},
+    '.chosen-select-deselect': {allow_single_deselect: true},
+    '.chosen-select-no-single': {disable_search_threshold: 10},
+    '.chosen-select-no-results': {no_results_text: 'Sorry, nothing here'},
+    '.chosen-select-width': {width: "95%"}
+ }
+ for (var selector in config){
+    $(selector).chosen(config[selector]);
+ }
+
+
  // In this code below we create the Front-end Javascript which "POSTS" our form data to our express server.
     // In essence, when the user hits submit, jQuery grabs all of the fields then sends a post request to our api
     // Our api recognizes the route (/api/tables)... and then runs the associated code (found in api-routes.js).
     // In this case the associated code "saves" the data to the table-data.js file or waitinglist-data.js file
 
-    $(".submit").on("click", function(){
+    $("#submit").on("click", function(){
+        function confirmForm(){
+            var valid = true;
+            $('.form-control').each(function(){
+                if ($(this.val() === '')
+                    valid = false;
+            });
 
-        // Here we grab the form elements
-        var newReservation = {
-            customerName: $('#reserve_name').val().trim(),
-            phoneNumber: $('#reserve_phone').val().trim(),
-            customerEmail: $('#reserve_email').val().trim(),
-            customerID: $('#reserve_uniqueID').val().trim()
-        };
+            if($(this).val() === "")
+                valid = false
+        })
+            return valid;
+        }
 
-        console.log(newReservation);
-
-        // This line is the magic. It's very similar to the standard ajax function we used.
-        // Essentially we give it a URL, we give it the object we want to send, then we have a "callback".
-        // The callback is the response of the server. In our case, we set up code in api-routes that "returns" true or false
-        // depending on if a tables is available or not.
-
-        // Here we get the location of the root page.
-        // We use this instead of explicitly saying the URL is localhost:3001 because the url will change when we deploy.
+        if (confirmForm() == true){
+            var playerInfo = {
+                playerName: $('#player_name').val().trim(),
+                playerPic: $('#player_pic').val().trim(),
+                playerScores: [$("#q1").val(), $("#q2").val(), $("#q3").val(), $("#q4").val(), $("#q5").val(), $("#q6").val(), $("#q7").val(), $("#q8").val(), $("#q9").val(), $("#q10").val()]                
+            };    
         var currentURL = window.location.origin;
 
-        $.post(currentURL + "/api/tables", newReservation,
-        function(data){
-
-            // If a table is available... tell user they are booked.
-            if(data == true){
-                alert("Yay! You are officially booked!")
-            }
-
-            // If a table is available... tell user they on the waiting list.
-            if(data == false){
-                alert("Sorry you are on the wait list")
-            }
-
-            // Clear the form when submitting
-            $('#reserve_name').val("");
-            $('#reserve_phone').val("");
-            $('#reserve_email').val("");
-            $('#reserve_uniqueID').val("");
-
+        $.post(currentURL + "/api/friends", playerInfo, function(data){
+            $("#matchName").text(data.name); //matchName is referring to an html element
+            $("#matchPic").attr("src", data.photo);
+            $("#resultsModal").modal('toggle');
         });
+    }
+    else{
+        alert("Hold on there, you have to make sure to fill out EVERYTHING! You sure you did that? Try again.");
+    }
 
 return false;
-
 });
+
+//function to run through results and compare differences
+
+var player1 = ["0", "0", "0", "0", "0"];
+var player2 = ["0", "0", "0", "0", "0"];
+var compare = ["0", "0", "0", "0", "0"];
+
+for (var i = 0; i < compare.length; i++){
+    player1 - player2
+}
+
+function(){
+    if ((player1 - player2) == -i)
+        Math.abs(x);
+}
+else{
+    console.log(compare);
+}
