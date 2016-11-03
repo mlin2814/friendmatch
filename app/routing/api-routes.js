951339c62adb9c4f6d5d72c57a0b0245b92c83b1
Matthew Lin <Matthew Lin>
2016-11-03 10:14:05 -0400

    Adjusted js and html files for refined formatting. Developing user data and storage

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -4,17 +4,13 @@ module.exports = function (app) {
 	// API GET Requests
 
 	app.get('/api/friends', function (req, res) {
-		res.json(friends);
+		res.json(surveyData);
 	});
 
 	app.post('/api/friends', function (req, res) {
 		// Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
 		// It will do this by sending out the value "true" have a table
-		if (friends.length < 5) {
-			tableData.push(req.body);
+			surveyData.push(req.body);
 			res.json(true); // KEY LINE
-		} else { // Or false if they don't have a table
-			waitListData.push(req.body);
-			res.json(false); // KEY LINE
-		}
+		
 	});
