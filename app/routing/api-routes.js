90197b61a66759372b9ceb1e95e9e39a74b02569
Matthew Lin <Matthew Lin>
2016-11-03 16:01:04 -0400

    Refined formatting and formula for user data

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,16 +1,31 @@
-var surveyData = require('../data/friends.js');
+var friends = require('../data/friends.js');
+var path = require('path');
+var parser = require('body-parser');
+
 
 module.exports = function (app) {
-	// API GET Requests
+	app.use(parser.json());
+    app.use(parser.urlencoded({extended: true}));
+    app.use(parser.text());
 
 	app.get('/api/friends', function (req, res) {
-		res.json(surveyData);
+		res.json(friends);
 	});
 
 	app.post('/api/friends', function (req, res) {
-		// Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-		// It will do this by sending out the value "true" have a table
-			surveyData.push(req.body);
-			res.json(true); // KEY LINE
-		
+		var answers = req.body;
+      	console.log(answers);
+      	friends.push(answers);
+
+		var player1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+		var player2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+		var compare = 0;
+
+			for (var i = 0; i < friends.length; i++){
+			    compare += Math.abs(player1[i] - player2[i]);
+			}
+		friends.push(req.body);
+			var bestResult = compare.indexOf(Math.min.apply(null,sum));
+			var match = friends[bestResult]
+			res.json(match); // KEY LINE	
 	});
--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -12,6 +12,9 @@ module.exports = function (app) {
 		res.sendFile(path.join(__dirname + '/../public/survey.html'));
 	});
 
+	app.get('/home', function (req, res) {
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+		
 	// If no matching route is found default to home
 	app.use(function (req, res) {
 		res.sendFile(path.join(__dirname + '/../public/home.html'));
